01587f7c72af5f0cec2fb015d6e564fdce1deff4
abc <jacksdevwork@gmail.com>
2021-02-22 14:50:09 -0800

    css set

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <h1>Slack build</h1>
+      
+      <Router>
+      <>
+        <Switch>
+          <Route path="/" exact>
+            <h1>Home page</h1>
+          </Route>
+        </Switch>
+      </>
+    </Router>
+
     </div>
   );
 }
