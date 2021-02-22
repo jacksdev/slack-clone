b4a1019847c187e6e047acd90cf4c7937f27625d
abc <jacksdevwork@gmail.com>
2021-02-22 13:05:00 -0800

    firebase set up

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAvyzDkEFhR9KvX-wNSfKF8gYUagd7JAtY",
+    authDomain: "slack-clone-a2ac2.firebaseapp.com",
+    projectId: "slack-clone-a2ac2",
+    storageBucket: "slack-clone-a2ac2.appspot.com",
+    messagingSenderId: "315031316562",
+    appId: "1:315031316562:web:c404ea3c3a59d8ee0fd78b"
+  };
