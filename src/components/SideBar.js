3ad5ba80967d1af6483fbb72d2ded77583f40a84
abc <jacksdevwork@gmail.com>
2021-02-25 19:45:14 -0800

    sideoption component set up

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -3,6 +3,17 @@ import styled from 'styled-components';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import CreateIcon from '@material-ui/icons/Create';
 
+import InsertCommentIcon from '@material-ui/icons/InsertComment'
+import InboxIcon from '@material-ui/icons/Inbox'
+import DraftsIcon from '@material-ui/icons/Drafts'
+import BookmarBorderIcon from '@material-ui/icons/BookmarkBorderOutlined'
+import FileCopyIcon from '@material-ui/icons/FileCopy'
+import PeaopleAltIcon from '@material-ui/icons/PeopleAlt'
+import AppsIcon from '@material-ui/icons/Apps'
+import ExpandLessIcon from '@material-ui/icons/ExpandLess'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+
+
 import SidebarOption from './SidebarOption'
 
 
@@ -19,10 +30,17 @@ function SideBar() {
                 </SidebarInfo>
                 <CreateIcon />
 
-                <SidebarOption />
             </SidebarHeader>
 
-            <SidebarOption />
+            <SidebarOption Icon={InsertCommentIcon} title="Threads" />
+            <SidebarOption Icon={InboxIcon} title="Mentions & Reactions" />
+            <SidebarOption Icon={DraftsIcon} title="Saved Items" />
+            <SidebarOption Icon={BookmarBorderIcon} title="Channel browser" />
+            <SidebarOption Icon={PeaopleAltIcon} title="People & user groups" />
+            <SidebarOption Icon={AppsIcon} title="Apps" />
+            <SidebarOption Icon={FileCopyIcon} title="File browser" />
+            <SidebarOption Icon={ExpandLessIcon} title="Show less" />
+            
         </SidebarContainer>
     )
 }
--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -2,11 +2,21 @@ import React from 'react'
 import styled from 'styled-components';
 
 
-function SidebarOption() {
+function SidebarOption({ Icon, title }) {
     return (
-        <div>
-            
-        </div>
+        <SidebarOptionContainer>
+
+            { Icon && <Icon fontSize="small" style={{ padding: 10 }} />}
+            { Icon ? (
+                <h3>{title}</h3>
+            ): (
+                <SideOptionChannel>
+                    <span>#</span> {title}
+                </SideOptionChannel>
+            ) }
+
+
+        </SidebarOptionContainer>
     )
 }
 
@@ -14,5 +24,26 @@ export default SidebarOption
 
 
 const SidebarOptionContainer = styled.div`
+    display: flex;
+    font-size: 12px;
+    align-items: center;
+    padding-right: 2px;
+    cursor: pointer;
+
+    :hover {
+        opacity: 0.9;
+        background-color: #340e36;
+    }
+
+    > h3 {
+        font-weight: 500;
+    }
+
+    > h3 > span {
+        padding: 15px;
+    }
+`
+
+const SideOptionChannel = styled.div`
 
 `
