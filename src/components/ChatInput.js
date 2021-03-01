0103526838da2a89d2581cc9074a80edf83dbb51
abc <jacksdevwork@gmail.com>
2021-03-01 15:34:04 -0800

    styled input component

--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -1,11 +1,47 @@
+import { Button } from '@material-ui/core';
 import React from 'react'
+import styled from 'styled-components';
 
-function ChatInput() {
+function ChatInput({ channelName, channelId }) {
+
+    const sendMessage = e => {
+        e.preventDefault();
+    };
     return (
-        <div>
+        <ChatInputContainer>
+            <form action="">
+                <input placeholder={`Message #Room`} />
+                <Button hidden type='submit' onClick={sendMessage}>
+                    SEND
+                </Button>
+            </form>
             
-        </div>
+        </ChatInputContainer>
     )
 }
 
 export default ChatInput
+
+
+const ChatInputContainer = styled.div`
+    border-radius: 20px;
+
+    > form {
+        position: relative;
+        display: flex;
+        justify-content:center;
+    }
+
+    > form > input {
+        position: fixed;
+        bottom: 30px;
+        width: 60%;
+        border: 1px solid gray;
+        padding: 20px;
+        outline: none;
+    }
+
+    > form > button {
+        display: none !important;
+    }
+`
